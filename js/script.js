af58893b5e738126753177cc634959176417fa64
Royce Le <roycele123@gmail.com>
2025-08-27 21:02:44 -0700

    Created a skeleton. Created index.html, css, & js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// This file is currently empty.
+// You can add JavaScript here later to add interactivity,
+// such as a simple image carousel, form validation, or a menu toggle for mobile.
+
+console.log("JavaScript file is linked and ready for coding!");
